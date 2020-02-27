aaaa1527f8eee5a77d5a9f7e1ab036e465c88504
Raquel Rodriguez <>
2020-02-26 20:10:29 -0500

    Adding previous lessons.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,115 @@
+// console.log("Hello World!!!");
+
+/* ****************************
+* VARIABLES AND DATA TYPES
+
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Teacher";
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var johnMark = "John and Mark";
+*/
+
+/*********************************
+ * VARIABLE MUTATION AND TYPE COERCION
+ 
+
+var firstName = "John";
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = "Teacher";
+isMarried = false;
+
+console.log(firstName + ' is a ' + 28 + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = "twenty eight";
+job = "driver";
+
+alert(firstName + ' is a ' + 28 + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt("What is his last name?");
+console.log(firstName + " " + lastName);
+*/
+
+/************************************
+* BASIC OPERATORS
+ 
+var year, yeaJohn, yearMark;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof "hey");
+var x;
+console.log(typeof x);
+*/
+
+/************************************
+* OPERATOR PRECEDENCE
+ */
+
+ var now = 2018;
+ var yearJohn = 1989;
+ var fullAge = 18;
+
+// Multiple operators 
+ var isFullAge = now - yearJohn >= fullAge; // true
+ console.log(isFullAge);
+
+// Grouping
+ var ageJohn = now - yearJohn;
+ var ageMark = 35;
+ var average = (ageJohn + ageMark) / 2;
+ console.log(average);
+
+ // Multiple assignments
+ var x, y;
+ x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+ console.log(x, y);
+
+ // More operators
+x *= 2; // x = x * 2
+console.log(x);
+x += 10; // x = x + 10
+console.log(x);
+x++ // x += 1 // x = x + 1
+console.log(x);
+
 // Coding challenge 1 - calculating BMI
 
 // 1. Store Mark's and John's mass and height in variables
